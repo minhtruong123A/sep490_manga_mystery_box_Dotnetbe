1bd27ccfd6b2c498d7dacfa1666f07aa2a223cc4
zacgnOoO <zachkey462003@gmail.com>
2025-07-28 14:05:19 +0700

    Update UpdateSellProductDto.cs

--- BusinessObjects/Dtos/Product/UpdateSellProductDto.cs
+++ BusinessObjects/Dtos/Product/UpdateSellProductDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace BusinessObjects.Dtos.Product
     public class UpdateSellProductDto
     {
         public string Id { get; set; }
+        [StringLength(300, MinimumLength = 10, ErrorMessage = "Description must be between 10 and 300 characters.")]
         public string Description { get; set; }
         public int Price { get; set; }
         public DateTime UpdatedAt { get; set; } = DateTime.Now;

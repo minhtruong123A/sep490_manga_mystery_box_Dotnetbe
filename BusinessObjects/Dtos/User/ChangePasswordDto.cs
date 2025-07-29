e84bc9f52b4d5ba5e97eb04f2f027408bc6fdf08
zacgnOoO <zachkey462003@gmail.com>
2025-07-29 23:25:26 +0700

    Fix bug change password

--- BusinessObjects/Dtos/User/ChangePasswordDto.cs
+++ BusinessObjects/Dtos/User/ChangePasswordDto.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace BusinessObjects.Dtos.User
 {
+
     public class ChangePasswordDto
     {
-/*        public string UserId { get; set; }*/
+        [Required(ErrorMessage = "Current password is required")]
         public string CurentPassword { get; set; }
+
+        [Required(ErrorMessage = "New password is required")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*(),.?\\:{}|<>]).{8,15}$",
+            ErrorMessage = "New password must be 8-15 characters, include uppercase, lowercase, number and special character")]
         public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
         public string ConfirmPassword { get; set; }
     }
+
 }

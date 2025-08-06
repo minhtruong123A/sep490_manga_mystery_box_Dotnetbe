1c849c2d8f6445236604d6bfa37e7e8ed75bd1e7
Truong Hai Minh <144296136+minhtruong123A@users.noreply.github.com>
2025-08-06 10:44:28 +0700

    chore: update success & cancel urls for PayOS service

--- Services/Service/PayOSService.cs
+++ Services/Service/PayOSService.cs
@@ -55,8 +55,8 @@ namespace Services.Service
                 amount,
                 description,
                 items,
-                cancelUrl: "https://yourfrontend.com/cancel",
-                returnUrl: "https://yourfrontend.com/success",
+                cancelUrl: "https://youtu.be/b3rNUhDqciM?si=mryd9IU_aOcrW82s",
+                returnUrl: "https://youtu.be/dQw4w9WgXcQ?si=5BxIvepyCzYv25hf",
                 expiredAt: expiredAt
             );
 
